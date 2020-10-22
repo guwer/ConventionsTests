a5def8c721aeea5e9ac78b440da39fc4cbbe6b48
Krzysiek <guvver@gmail.com>
2020-10-22 23:19:33 +0200

    Init repository

---
+++ ConventionsTests/ConventionsHelper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace ConventionsTests
+{
+    public class ConventionsHelper
+    {
+        public static IEnumerable<string> GetSourceFiles()
+        {
+            var root = Path.GetFullPath(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "..\\..\\..\\..\\"));
+            var sourceFiles = Directory.EnumerateFiles(root, "*.cs", SearchOption.AllDirectories).Where(p => !p.Contains("obj"));
+
+            return sourceFiles;
+        }
+    }
+}
---
+++ ConventionsTests/DateTimeTests.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System;
+using System.IO;
+using System.Linq;
+using Xunit;
+
+namespace ConventionsTests
+{
+    public class DateTimeTests
+    {
+        [Fact]
+        public void DateTimeNow_IsNeverUsed()
+        {
+            var sourceFiles = ConventionsHelper.GetSourceFiles();
+
+            foreach (var file in sourceFiles)
+            {
+                var content = File.ReadAllText(file);
+
+                var tree = CSharpSyntaxTree.ParseText(content);
+                var root = tree.GetRoot();
+                var memberAccesses = root.DescendantNodes()
+                    .OfType<MemberAccessExpressionSyntax>()
+                    .ToList();
+
+                var datetimeNowInvocations = memberAccesses
+                    .Where(m => m.Expression.Parent.ToString().Equals("datetime.now", StringComparison.OrdinalIgnoreCase))
+                    .ToList();
+
+                Assert.Empty(datetimeNowInvocations);
+            }
+        }
+    }
+}
